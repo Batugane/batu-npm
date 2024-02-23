034654399ed63c5aa141d62a906df892d5824d58
batuhanDuras <batugane@gmail.com>
2024-02-23 10:24:08 +0300

    first commit

---
+++ index.mjs
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+import chalk from "chalk";
+
+console.log(chalk.blue("Batuhan Duras - Biography\n"));
+
+console.log(chalk.green("Profile:"));
+console.log(
+  "High-achieving 3rd-year Information Systems and Technologies student..."
+);

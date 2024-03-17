d4ab09552cf49e05723b4ed898b3c593c90ca52c
batuhan duras <21129845+Batugane@users.noreply.github.com>
2024-03-17 13:21:19 +0300

    feat: changed bio

--- index.mjs
+++ index.mjs
@@ -5,6 +5,5 @@ import chalk from "chalk";
 console.log(chalk.blue("Batuhan Duras - Biography\n"));
 
 console.log(chalk.green("Profile:"));
-console.log(
-  "High-achieving 3rd-year Information Systems and Technologies student..."
-);
+console.log("3rd-year Information Systems and Technologies student...");
+console.log("full-stack developer, and a tech enthusiast.");

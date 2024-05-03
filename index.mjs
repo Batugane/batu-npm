b5bad27f5d62f790df968e7c2653bf21928ac933
batuhan duras <21129845+Batugane@users.noreply.github.com>
2024-05-03 16:34:36 +0300

    feat: update package version to 1.0.6

--- index.mjs
+++ index.mjs
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-import chalk from "chalk";
+import chalk from 'chalk'
 
-console.log(chalk.blue("Batuhan Duras - Biography\n"));
+console.log(chalk.blue('Batuhan Duras - Biography\n'))
 
-console.log(chalk.green("Profile:"));
-console.log("3rd-year Information Systems and Technologies student...");
-console.log("full-stack developer, and a tech enthusiast.");
+console.log(chalk.green('Profile:'))
+console.log('3rd-year Information Systems and Technologies student.')
+console.log('full-stack developer, and a tech enthusiast.')

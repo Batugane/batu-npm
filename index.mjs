6da118e85087a2413da0400e7b53fead9ac234f1
batuhan duras <21129845+Batugane@users.noreply.github.com>
2024-12-02 21:59:40 +0300

    1.0.7

--- index.mjs
+++ index.mjs
@@ -5,5 +5,5 @@ import chalk from 'chalk'
 console.log(chalk.blue('Batuhan Duras - Biography\n'))
 
 console.log(chalk.green('Profile:'))
-console.log('3rd-year Information Systems and Technologies student.')
-console.log('full-stack developer, and a tech enthusiast.')
+console.log('Senior Information Systems and Technologies student.')
+console.log('full-stack developer')
